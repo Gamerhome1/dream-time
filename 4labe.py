08fa00d6c4ebd948e4bb8662587c9c656bfa176f
Gamerhome1 <98109050+Gamerhome1@users.noreply.github.com>
2022-02-01 20:37:34 +0300

    никита кузьмин

--- 4labe.py
+++ 4labe.py
@@ -20,6 +20,13 @@ def dict_way_size(way):
 
 
 def analysis(dict1):
+  duplicate = {}
+  for key1 in dict1:
+    for key2 in dict1:
+        if key1[key1.rfind("\\"):] == key2[key2.rfind("\\"):] and dict1[key1] == dict1[key2] and key1 != key2:
+            if key1 not in duplicate:
+                duplicate.update({key1: dict1[key1]})
+  return duplicate
 
 
 
