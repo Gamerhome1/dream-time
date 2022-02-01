2514056700f7bc5fcb54b85549631d08a96142ec
Aleksandr90210 <98108166+Aleksandr90210@users.noreply.github.com>
2022-02-01 09:29:05 +0300

    Create 4labe.py

---
+++ 4labe.py
@@ -0,0 +1,32 @@
+import os
+
+
+def dir():
+  pass
+  
+  
+  
+  
+  
+  
+  
+def dict_way_size(way):
+ pass
+
+
+
+
+
+
+
+def analysis(dict1):
+
+
+
+
+  
+  
+  
+  
+  
+def dupclicate(duplicate):
